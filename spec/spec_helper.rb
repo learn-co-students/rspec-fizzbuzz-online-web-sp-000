5e43765aa1f9b5bdc91c7d0cb3fca8f799e39d89
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-09-26 20:18:23 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,8 @@
 # specs live under a `spec` directory, which RSpec adds to the `$LOAD_PATH`.
 # Require this file using `require "spec_helper"` to ensure that it is only
 # loaded once.
-#
+require_relative './spec_helper.rb'
+require_relative '../fizzbuzz.rb'
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 # NOTE: If you uncomment the following line,
