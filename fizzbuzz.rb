dd0d7d6398b99332d27ef6c3b8a2e9c5add25c42
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-10-16 22:18:41 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,18 @@
-# Don't forget! This file needs to be 'required' in its spec file
+   # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
-  end
+  if int % 3 == 0 && int % 5 == 0  # if the number int is divisible by 3 AND 5
+    "FizzBuzz" # Go fizzbuzz 
     
-    if int % 5 == 0 # if the number int is divisible by 3
+    elsif int % 5 == 0 # if the number int is divisible by 3
     "Buzz" # Go buzz
-  end
     
-    if int % 3 == 0 && int % 5 == 0  # if the number int is divisible by 3 AND 5
-    "FizzBuzz" # Go fizzbuzz
+    elsif  int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+    
+  else
+    nil
   end
 end
 
