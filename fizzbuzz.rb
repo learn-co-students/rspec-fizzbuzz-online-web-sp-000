1d1ef40399c7ff7ffe1741c3129a3d74a9e1b8ba
Kimberly Rouse <ksrouse94@gmail.com>
2019-01-05 19:30:26 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if (num % 3 == 0 && num % 5 == 0)
-    "Fizzbuzz"
+    "FizzBuzz"
   elsif num % 5 == 0
     "Buzz"
   elsif num % 3 == 0
