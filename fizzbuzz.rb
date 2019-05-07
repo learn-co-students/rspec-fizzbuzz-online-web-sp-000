6ecc6bcf136f969ad06c6794660313bae63ba5d5
Sean Barile <aquariussb84@gmail.com>
2019-05-07 20:43:16 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,13 @@
-def fizzbuzz(num)
-  if num % 3 == 0 and num % 5 == 0
+# Don't forget! This file needs to be 'required' in its spec file
+# See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
-  elsif num % 3 == 0 
+  elsif int % 3 == 0
     "Fizz"
-  elsif num % 5 == 0 
+  elsif int % 5 == 0
     "Buzz"
+  else int != 3 || 5
+    nil
   end
 end
