9d60995169d0f4cc4d9461ca142abb067700a6bc
Connor Cowan <ccowan003@comcast.net>
2019-05-14 22:31:42 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(number)
+def fizzbuzz(integer)
 
-  if (number % 3 == 0)
+  if (integer % 3 == 0)
     return "Fizz"
-  elsif (number % 5  == 0)
+  elsif (integer % 5  == 0)
     return "Buzz"
-  elsif (number % 15 == 0)
+  elsif (integer % 3 == 0 && integer % 5 == 0)
     return "FizzBuzz"
   else
     return nil
-  end
+end
