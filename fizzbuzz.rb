2391bdda5a271061c04dec95d8348c667064b123
C. Leow <chienleow1@gmail.com>
2019-05-31 05:17:57 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require_relative 'spec'
+def fizzbuzz(int)
+  if int % 3 == 0
+    return "fizz"
+    elsif int % 5 == 0
+    return "buzz"
+    elsif int % 3 == 0 && int % 5 == 0
+      return "fizzbuzz"
+    else 
+      return "nil"
+end
+end
