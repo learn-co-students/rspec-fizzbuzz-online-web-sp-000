a8eff9251805c46a04b3473efb44124db01ae9d1
github username <github email address>
2019-06-03 11:04:22 -0400

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
+
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
 if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
+    elsif int % 5 == 0
+      "Buzz"
+    elsif int % 15 == 0
+      "FizzBuzz"
+    else
+      nil
   end
 end
 
@@ -10,3 +17,4 @@ end
 fizzbuzz(3) # => You should see a return of "Fizz"
 fizzbuzz(5) # => You should see a return of nil
 fizzbuzz() 
+
