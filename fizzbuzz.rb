62b4904c2ddcc96ecffb10d4001472be09503845
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-06-13 00:18:04 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def Fizzbuzz(x)
+  if x % 3 == 0
+    puts "Fizz"
+  elseif x % 5 == 0
+    puts "Buzz"
+  elseif x % 3 && x % 5 == 0
+    puts "FizzBuzz"
+  else
+    nil
+  end
