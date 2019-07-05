a0cf5fc220f0f67327410f2c0d59a5b78f728456
Christopher Dent <christopherdent01@gmail.com>
2019-07-05 02:51:29 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,15 @@
-def fizzbuzz(int)
-  
- if int % 3 == 0
-   if int % 5 == 0
-     "FizzBuzz"
-   else
-     "Fizz"
-   end 
-   
- elsif int % 5 == 0
+def fizzbuzz(number)
+
+if number % 15 == 0
+ "FizzBuzz"
+ elsif number % 3 == 0 
+ "Fizz"
+ elsif number % 5 == 0
   "Buzz"
-    
- else 
-   nil 
-  
-  end
+
+else
+nil
+end
+
 
 end
