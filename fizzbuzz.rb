9d75a95d7490210aaf59c5ba8cf9d3d4bb1ead47
Andrew Williams <1williamsaj@gmail.com>
2019-08-20 23:53:30 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(number) #First, we define our method and include an argument.
+  if (number % 3 == 0) && (number % 5 == 0) #Second, we want to make our main condition all encompassing. Thus we test for both first.
+    return "FizzBuzz" #We use return to avoid the 'nil' that comes with using 'puts'.
+  elsif number % 3 == 0 #Third, move on to just one case.
+    return "Fizz"
+  elsif number % 5 == 0 #Fourth, final true case.
+    return "Buzz"
+  else #If all else fails, return the 'nil' as provided in the instructions.
+    return nil
+  end
+
+end
