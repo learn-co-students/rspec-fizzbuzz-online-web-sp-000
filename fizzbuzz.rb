3746a8b0f5fd830f3c7743adfc82c5b59dfce92d
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-12 06:01:46 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+## learn is basically rspec -> remember this.
+
+def fizzbuzz (number)
+  fizz = number % 3
+  buzz = number % 5
+  if fizz == 0 && buzz == 0
+    return "FizzBuzz"
+  elsif buzz == 0
+    return "Buzz"
+  elsif fizz == 0
+    return "Fizz"
+  else
+    return nil
+  end
+end
+
