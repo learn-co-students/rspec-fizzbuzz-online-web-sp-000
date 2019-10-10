7dd3fc80305d08930cc137d9a66f35799843d6ba
Chad Alan Lengefeld <chadlengefeld@gmail.com>
2019-10-10 18:57:57 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
-require_relative '../fizzbuzz.rb'
+
+
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if (int % 3) == 0
-  puts  "Fizz"
+
+  if (int % 3 == 0) && (int % 5 == 0)
+    "FizzBuzz"
+
+  elsif int % 3 == 0
+  "Fizz"
+
+elsif int % 5 == 0
+    "Buzz"
+
+  else (int % 3 != 0) || (int % 5 != 0)
+    nil
+    end
   end
-end
