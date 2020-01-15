316a8335ad5b4003e31f23ea4583beb93d4569a0
looping-android-0302 <jennifer.pfeffer127@gmail.com>
2020-01-15 18:19:27 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(int)
     puts "Fizz"
   elsif int % 5 == 0 
     puts "Buzz" 
-  elsif int % 5 == 0 and number % 3 == 0 
+  elsif int % 5 == 0 and int % 3 == 0 
     puts "FizzBuzz" 
   else return nil
 end
