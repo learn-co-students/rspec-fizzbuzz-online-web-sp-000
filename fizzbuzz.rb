03eb515ccad2a3df054eac73bcb5a51dac829203
Austin Rhoads <austinrrhoads@ymail.com>
2020-01-27 00:15:06 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  x = num.to_i
+  div3 = x % 3
+  div5 = x % 5
+  if div3 == 0 && div5 == 0
+    return "FizzBuzz"
+
+  elsif div5 == 0
+    return "Buzz"
+  elsif   div3 == 0
+      return "Fizz"
+  else
+    return nil
+end
+end
