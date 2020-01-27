478ec5f06f567b1081fccdc149f2e517c4e7ec00
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-01-27 20:23:26 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz  (number)
-  if number/3 = (number/3).to_i
+  if int % 3 == 0
     print "Fizz"
-  elsif number/5 = (number/5).to_i
+  elsif int % 5 == 0
     print "Buzz"
-  elsif number/15 = (number/15).to_i
+  elsif int % 15 == 0
     print "FizzBuzz"
   else
 
