c2081b9cb8c8a6b0c49d967730046319f09887aa
Jacob W Jones <jacobwadejones76@gmail.com>
2020-02-11 18:59:54 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(any)
+  if any % 3 == 0 && any % 5 == 0
+      return "FizzBuzz"
+    elsif any % 3 == 0
+        return "Fizz"
+      elsif any % 5 == 0
+          return "Buzz"
+    else
+      puts "Not divisible by 3 or 5"
+  end 
+
+end
