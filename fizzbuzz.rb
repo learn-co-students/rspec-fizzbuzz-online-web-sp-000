1d548cea29c8a350ddaf389da899d3f325631838
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-21 12:50:43 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 != 0
+    "Fizz"
+  elsif int % 5 == 0 && int % 3 != 0
+    "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  else int % 3 != 0 && int % 5 != 0
+    nil 
+  end
+end
+
+puts fizzbuzz(1)
+puts fizzbuzz(3)
+puts fizzbuzz(5)
+puts fizzbuzz(15)
+puts fizzbuzz(30)
