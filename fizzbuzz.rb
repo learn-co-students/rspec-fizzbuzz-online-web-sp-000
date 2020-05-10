9c093e5ae5fc0f0c5f5387e535366b2a24ddb488
elsa chen <snowing222@gmail.com>
2020-05-10 10:18:32 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,17 +2,26 @@
 # See README.md for instructions on how to do this
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int%3==0
-    if int%5==0
-      "FizzBuzz"
-    else
-      "Fizz"
-    end
+#def fizzbuzz(int)
 
-  elsif int%5==0
-    "Buzz"
-  else
+def fizzbuzz(int)
+  string=""
+  if int%3==0
+    string+="Fizz"
+
+  end
+
+  if int%5==0
+    string+="Buzz"
+
+  end
+
+
+  if string!=""
+    string
+  elsif
     nil
   end
+
+
 end
