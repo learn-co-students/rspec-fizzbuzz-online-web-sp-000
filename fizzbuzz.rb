ee70c60f84f0b84b8e6d8327315d5c9a12fff35b
Marcel Rico <personal.mr4@gmail.com>
2020-05-24 15:48:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(n)
+  if (n % 3 != 0) && (n % 5 != 0)
+    return nil
+  else
+    str = ""
+    if(n % 3 == 0)
+      str += "Fizz"
+    end
+    if(n % 5 == 0)
+      str+= "Buzz"
+    end
+    return str
+  end
+end
