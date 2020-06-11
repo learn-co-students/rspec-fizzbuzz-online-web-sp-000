6428584f0d0461b455da288fa998826ae4e64028
Leslie G <lesliegonzalez@me.com>
2020-06-11 01:26:52 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,13 @@
 
 
 def fizzbuzz(number)
-  if (number % 3 == 0) && (number % 5 == 0)  
+   if (number % 3 == 0) && (number % 5 == 0)  
    "Fizzbuzz"
    elsif number % 5 == 0
    "Buzz"
    elsif number % 3 == 0
    "Fizz"
+   else
+   nil
   end
 end
