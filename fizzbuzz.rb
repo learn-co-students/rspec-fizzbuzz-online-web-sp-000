27775b640cdd7d716a13e7f30988e3803a3fc731
nisticism <nistacular@gmail.com>
2020-07-25 01:17:38 -0700

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,9 @@
 def fizzbuzz(number)
     if (number%3 == 0 && number%5 == 0)
         return "FizzBuzz"
-    end
-    if (number%3 == 0)
+    elsif (number%3 == 0)
         return 'Fizz'
-    end
-    if (number%5 == 0)
+    elsif (number%5 == 0)
         return 'Buzz'
     end
 end
