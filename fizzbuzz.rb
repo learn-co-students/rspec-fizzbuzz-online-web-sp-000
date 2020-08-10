44b02144e8214e6295cd47a660c180d373bd1b29
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-08-10 23:00:16 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num % 5 == 0 && num % 3 == 0
+    "FizzBuzz"
+  else if num % 5 == 0
+    "Buzz"
+  else if num % 3 == 0
+    "Fizz"
+  end
+  end
+  end
+end
