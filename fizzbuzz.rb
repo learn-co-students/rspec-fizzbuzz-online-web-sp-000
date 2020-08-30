8771a9c7ff221a840cbb4d85f8dd9595a7bf5bec
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-30 18:37:39 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,43 +1,18 @@
-require 'pry'
 
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-#def fizzbuzz(int)
-#  if int % 3 == 0 # if the number int is divisible by 3
-#    "Fizz" # Go fizz
-#    elsif int % 5 == 0 
-#    "Buzz" 
-#    elsif (int%5 === 0 && !int%3 === 0)  
-#    "FizzBuzz"
-#  else 
-#    nil 
-#  end
-#end
-
 
 
 def fizzbuzz(i)
-  if i % 3 == 0
-    "Fizz"
+  if i % 3 == 0 && i % 5 == 0
+    "FizzBuzz"
   elsif i % 5 == 0
     "Buzz"
-  elsif i % 3 == 0 && i % 5 == 0
-    "FizzBuzz"
+  elsif i % 3 == 0
+    "Fizz"
+  else 
+    nil 
   end
 end
 
-#def fizzbuzz(number)
-#  divisibleBy3 = (number % 3 == 0)
-#  divisibleBy5 = (number % 5 == 0)
-#  
-#  if divisibleBy3 && divisibleBy5
-#    puts "FizzBuzz"
-#  elsif divisibleBy3
-#    puts "Fizz"
-#  elsif divisibleBy5
-#    puts "Buzz"
-#  else 
-#    puts number
-#  end 
-#end
