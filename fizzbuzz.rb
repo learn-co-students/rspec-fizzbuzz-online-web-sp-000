c0bbf9fc366b507bcc724013ba244fe74c871a67
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-10-08 05:39:19 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require 'pry'
+
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    return "FizzBuzz"
+    #binding.pry
+  elsif
+    number % 5 == 0
+    "Buzz"
+  elsif
+    number % 3 == 0
+    "Fizz"
+  else
+    nil
+  end
+end
