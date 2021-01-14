169c9a341e72ff7116049fcd86913532cdc532f5
Bryce B <bryce.boddy@gmail.com>
2021-01-14 13:25:44 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  string = ""
+  string += "Fizz" if (num % 3 == 0)
+  string += "Buzz" if (num % 5 == 0)
+  return string unless string.empty?
+  return nil
+end
