dcdbf41f24313db0f29fa65bd53949c0a746b5d4
Lj <tnedutsl457892@gmail.com>
2021-01-30 04:58:14 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    return "FizzBuzz"
+    "FizzBuzz"
   elsif int % 3 == 0
-    return "Fizz"
+    "Fizz"
   elsif int % == 0
-    return "Buzz"
+    "Buzz"
   else
     nil
   end
