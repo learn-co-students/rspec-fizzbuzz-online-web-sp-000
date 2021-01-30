94e108155f3faad71bce09fa460e90912c563019
Lj <tnedutsl457892@gmail.com>
2021-01-30 04:32:55 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num / 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif num / 5 == 0
-    puts "Buzz"
+    return "Buzz"
   elsif num / 3 == 0 && num / 5 == 0
-    puts "FizzBuzz"
+    return "FizzBuzz"
   end
 end
