40eccc1fdec5af73e818e2bc05edca40f507d421
Nathaniel Adam <daamnathaniel@gmail.com>
2021-03-28 04:13:29 -0500

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+
+
+def fizzbuzz(n)
+    case [n % 3 == 0, n % 5 == 0]
+    when [true, true] then "FizzBuzz"
+    when [true, false] then "Fizz"
+    when [false, true] then "Buzz"
+    else nil
+    end
+  end
